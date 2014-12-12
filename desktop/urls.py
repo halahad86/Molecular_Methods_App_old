310932a516101d0608f10ff65f6a9329ba04c508
Matt Frost <1102445f@student.gla.ac.uk>
2014-12-12 17:12:23 +0000

    Added more views and a login feature,  can't get the css to load properly yet though.  Still working on it :)

--- desktop/urls.py
+++ desktop/urls.py
@@ -5,7 +5,15 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
-        url(r'^admin/', include(admin.site.urls)),)
+        url(r'^index/$', views.index, name='index'),
+        url(r'^glossary/$', views.glossary, name='glossary'),
+        url(r'^labs/$', views.labs, name='labs'),
+        url(r'^admin/$', include(admin.site.urls)),
+        url(r'^register/$', views.register, name='register'),
+        url(r'^login/$', views.user_login, name='login'),
+        url(r'^logout/$', views.user_logout, name='logout'),
+
+)
 
 
 if settings.DEBUG:
