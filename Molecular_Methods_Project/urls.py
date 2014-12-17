5cd9dd208fc9ff38fb7d4c18a37ce55267bed000
p.l.dalziel@gmail.com <pldalzl>
2014-12-17 00:03:46 +0000

    Added new  images lab for lab icons

--- Molecular_Methods_Project/urls.py
+++ Molecular_Methods_Project/urls.py
@@ -12,10 +12,10 @@ urlpatterns = patterns('',
     # url(r'^Molecular_Methods_Project/', include('Molecular_Methods_Project.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+   url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+   url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^desktop/', include('desktop.urls', namespace='desktop')),
+   url(r'^desktop/', include('desktop.urls', namespace='desktop')),
 )
